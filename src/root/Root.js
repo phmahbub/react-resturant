6a374a82b98b184a784c455791144d1a8e41ef56
B. M. Mahbubul Alam <phmahbub@live.com>
2022-10-17 16:16:47 +0600

    context api created in two files UserContext and in Root file

--- src/root/Root.js
+++ src/root/Root.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import Footer from '../components/Footer';
 import NavBar from '../components/NavBar/NavBar';
 import {Outlet, useLoaderData} from 'react-router-dom'
 
+const ProductContext = createContext([])
 
 const Root = () => {
-    const user = useLoaderData()
-    console.log(user)
+   
+    const products = useLoaderData()
+    console.log(products)
     return (
         <div className='px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8'>
-            <NavBar />
-            <Outlet user={user}/>
-            <Footer/>
+            <ProductContext value={products}>
+                <NavBar />
+                <Outlet />
+                <Footer/>
+            </ProductContext>
+            
         </div>
     );
 };
